aea5670ffac55dd21c8d5bd1110160a1b5231f3c
ngugimuchangi <tech.curious14@gmail.com>
2022-04-08 15:58:58 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,9 @@
-able File  21 lines (21 sloc)  295 Bytes
-   
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   * Description:Prints all possible combinations of single-digit numbers
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description:Prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
