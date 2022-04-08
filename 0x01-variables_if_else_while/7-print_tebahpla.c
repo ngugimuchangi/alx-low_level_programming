0040f2d09135535e81f5b181c90011f4e83cf5a4
ngugimuchangi <tech.curious14@gmail.com>
2022-04-08 15:52:28 -0700

    Task 5 to 9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+able File  21 lines (21 sloc)  295 Bytes
+   
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Description:Prints all possible combinations of single-digit numbers
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	
+	return (0);
+}
