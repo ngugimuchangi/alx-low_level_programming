e181ae2d653d70bdcd65d6868b9ce7b47a862012
ngugimuchangi <tech.curious14@gmail.com>
2022-04-12 22:49:43 +0000

    Check lower and uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+/**
+ * _islower - check if lower case
+ * @c: character to check
+ * Return: 1 equal success, 0 equals failure
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,11 @@
  *
  * print_alphabet - prints alphabets plus new line
  *
- * print_alphabet_x10
+ * print_alphabet_x10 - prints alphabets in new line 10 times
+ *
+ * int _islower - checks if character is lowercase and return 1, else return 0
+ *
+ * int _isalpha - checks if character is lower or uppercase and return 1, else return 0
  */
 int _putchar(char c);
 
@@ -12,3 +16,5 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 
 int _islower(int c);
+
+int _isalpha(int c);
