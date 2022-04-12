44c2b0e02789cde410205a1d3d180ca1ef4b27bb
ngugimuchangi <tech.curious14@gmail.com>
2022-04-12 22:23:40 +0000

    Check if letter is zero

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check if lower case
+ *
+ * Return: 1 equal success, 0 equals failure
+ */
+int _islower(int c);
+{
+	if (c => 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
