fc132d395d07456ee71804d663e0e64e13d46d43
ngugimuchangi <tech.curious14@gmail.com>
2022-04-12 12:08:37 -0700

    Print alphabet 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,11 @@
  * _putchar - writes the chacter c to standard out
  *
  * print_alphabet - prints alphabets plus new line
+ *
+ * print_alphabet_x10
  */
 int _putchar(char c);
+
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
