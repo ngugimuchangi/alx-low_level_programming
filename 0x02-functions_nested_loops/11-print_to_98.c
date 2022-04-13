a0315cec4e156cd899f2f2594b67493100fac953
ngugimuchangi <tech.curious14@gmail.com>
2022-04-13 01:59:01 +0000

    Numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  */
 void print_to_98(int n)
 {
-	for (n, n >= 98, n--)
+	for (n; n >= 98; n--)
 	{
 	_putchar(n + '0');
 	if (n = 98)
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	_putchar(' ');
 	}
 	}
-	for (n, n <= 98, n++)
+	for (n; n <= 98; n++)
 	{
 	_putchar(n + '0');
 	if (n = 98)
