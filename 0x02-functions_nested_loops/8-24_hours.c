bfb25f8b6c332bd321c0286356afeff53d3e350b
ngugimuchangi <tech.curious14@gmail.com>
2022-04-13 00:54:16 +0000

    Jack Bauer 24 hour countdown

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,12 +6,11 @@
  */
 void jack_bauer(void)
 {
-	int m = 0;
-	int h = 0;
+	int h, m;
 
-	while (h < 24)
+	for (h = 0; h < 24; h++)
 	{
-	while (m < 60)
+	for (m = 0; m < 60; m++)
 	{
 	_putchar(h / 10 + '0');
 	_putchar(h % 10 + '0');
@@ -19,8 +18,6 @@ void jack_bauer(void)
 	_putchar(m / 10 + '0');
 	_putchar(m % 10 + '0');
 	_putchar('\n');
-	m++;
 	}
-	h++;
 	}
 }
