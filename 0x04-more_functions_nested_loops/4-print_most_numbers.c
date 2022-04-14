f135f504d4738d83f1e8c9854c419aaf33aca473
ngugimuchangi <tech.curious14@gmail.com>
2022-04-14 17:30:28 +0000

    Update files for task 4 and 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 and skips 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 0 to 14 on new line 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j / 10 != 0)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
