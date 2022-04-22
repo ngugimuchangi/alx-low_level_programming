5029944ae7d9affcf293b49637363c8034369c2a
ngugimuchangi <tech.curious14@gmail.com>
2022-04-22 14:26:40 +0000

    Leet code fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,34 +6,24 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
 
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; a[j]; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-		}
-		else
-		{
-			continue;
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+			else
+			{
+				continue;
+			}
 		}
 	}
+
 	return (str);
 }
