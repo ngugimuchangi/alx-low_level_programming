69b136a738102f8a35029b5d8743d74908ed5648
ngugimuchangi <tech.curious14@gmail.com>
2022-04-24 09:03:21 +0000

    Fixed code

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,7 +8,7 @@
  */
 void print_buffer(char *b, int size)
 {
-	int a, i = 0, j, k, l, m = 0, n;
+	int i = 0, j, k;
 
 	if (size <= 0)
 	{
@@ -19,22 +19,13 @@ void print_buffer(char *b, int size)
 		while (i < size)
 		{
 			printf("%08x: ", i);
+			for (k = 0; k < 10; k++)
+			{
+				(k + i < size) ? printf("%02x", b[i + k]) : printf("  ");
+				if (k % 2 != 0) 
+					printf(" ");
+			}
 
-			for (k = 0, n = 0; k <= 9 && m < size;)
-			{
-				for (l = 0; l < 2 && m < size; m++, k++, l++, n++)
-				printf("%02x", (unsigned char)b[m]);
-				printf(" ");
-				a = n;
-			}
-			for (; n < 10 && a % 2 == 0;  n += 2)
-				printf("     ");
-			if (n < 10 && a % 2 != 0)
-			{
-				printf(" ");
-				for (; n < 10; n += 1)
-				printf("  ");
-			}
 			for (j = 0; j <= 9 && i < size; i++, j++)
 			(b[i] >= 0 && b[i] <= 31) ? printf(".") : printf("%c", b[i]);
 			printf("\n");
