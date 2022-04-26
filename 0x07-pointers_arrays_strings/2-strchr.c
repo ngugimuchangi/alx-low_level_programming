727ecb4496ab98721c2d0c4974679ef8261d9102
ngugimuchangi <tech.curious14@gmail.com>
2022-04-26 11:32:04 +0000

    Fixed error with return value

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,13 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *find;
 
 	for (i = 0; *(s + i); i++)
 	{
 		if (*(s + i) == c)
 		{
-			find = (s + i);
-			return (find);
+			return (s + i);
 		}
 	}
-	return (NULL);
+	return (0);
 }
