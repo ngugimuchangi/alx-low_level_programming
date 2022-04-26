2f8bf6b940b5dbbdfbf13ea01d9e35b2142011b7
ngugimuchangi <tech.curious14@gmail.com>
2022-04-26 14:49:37 +0000

    Strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr -find the first occurrence of a substring
+ * @haystack: string to search in.
+ * @needle: substring to find.
+ * Return: pointer to the substring on success, otherwise null.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; *(haystack + i); i++)
+	{
+		if (*(haystack + i) == *(needle + 0))
+		{
+			for (j = 0; *(needle + j); j++)
+			{
+				if (*(haystack + i + j) != *(needle + j))
+					break;
+			}
+		if (*(haystack + i + j) == *(needle + j))
+			return ((haystack + i));
+		}
+	}
+	return (0);
+}
