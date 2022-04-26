7a425f553749629181cee39fb9543f187e3e1a29
ngugimuchangi <tech.curious14@gmail.com>
2022-04-26 16:25:48 +0000

    Temp fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,18 +7,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j;
+	unsigned int i, j, l;
 
-/*	l = 0; while (*(needle + l)) l++; */
+	l = 0; 
+	while (*(needle + l))
+		l++;
 	for (i = 0; *(haystack + i); i++)
 	{
-		for (j = 0; *(needle + j); j++)
+		for (j = 0; *(needle + j) && *(haystack + i + j); j++)
 		{
 			if (*(haystack + i + j) != *(needle + j))
 				break;
 		}
-		if (*(haystack + i + j) == *(needle + j))
+		if (j == l)
 			return ((haystack + i));
 	}
-	return ("NULL");
+	return (0);
 }
