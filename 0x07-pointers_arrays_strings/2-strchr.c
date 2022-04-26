1815db4073fb4accc6cefe440809a736cb071cc2
ngugimuchangi <tech.curious14@gmail.com>
2022-04-26 11:15:34 +0000

    Set return to NULL

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; s[i]; i++)
 	{
 		if (*(s + i) == c)
 		{
 			return ((s + i));
 		}
 	}
-	return ("NULL");
+	return (NULL);
 }
