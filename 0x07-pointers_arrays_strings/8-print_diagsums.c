4a590ae194a6a6b4c9bc3c5aa63896a925871aef
ngugimuchangi <tech.curious14@gmail.com>
2022-04-26 19:13:28 +0000

    Betty formatting fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum += *(a + i * size + i);
-		sum2 += *(a + (i * size) + (size - 1- i));
+		sum2 += *(a + (i * size) + (size - i - 1));
 	}
 	printf("%d, %d\n", sum, sum2);
 }
