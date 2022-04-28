564692bcde2c3367002ddf1ecdf6008a9b4c5d98
ngugimuchangi <tech.curious14@gmail.com>
2022-04-28 20:09:13 +0000

    Find squareroot of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sqrt - finds squareroot of a number
+ * @x: starting point
+ * @y: number
+ * Return: square root on success, -1 otherwise
+ */
+int _sqrt(int x, int y)
+{
+	if (x > y)
+		return (-1); 
+	if (y < 0)
+		return (-1);
+	if ((x * x) == y)
+		return (x);
+	return (_sqrt(x + 1, y));
+}
+
+/**
+ * _sqrt_recursion - finds the squareroot through recursion
+ * @n: number to find squareroot
+ * Return: squareroot on success, -1 otherwise
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(1, n));
+}
