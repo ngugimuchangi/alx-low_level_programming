55db046a2ab018ae3228322c456e4b996f7d8783
ngugimuchangi <tech.curious14@gmail.com>
2022-05-06 20:43:08 +0000

    Duplicate string and return pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - returns pointer to new duplicate string
+ * @str: string to duplicate.
+ * Return: pointer to str on success, NULL on failure.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *ptr;
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+		i++;
+	i++;
+	ptr = malloc(i * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (j = 0; str[j]; j++)
+		ptr[j] = str[j];
+	return (ptr);
+}
