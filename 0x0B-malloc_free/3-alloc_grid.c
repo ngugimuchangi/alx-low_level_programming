ed82a35ca90c05f65df7f68d34be3fafc9a32760
ngugimuchangi <tech.curious14@gmail.com>
2022-05-06 22:20:49 +0000

    Allocated memory to 2-D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - allocates block memory for 2-D integer array
+ * @width: array width
+ * @height: array height
+ * Return: pointer to the array on success, NULL on failure.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ptr = (int **) malloc(height * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	}
+	return (ptr);
+}
