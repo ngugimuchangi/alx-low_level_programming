238c408618d14e45ea4d1563d11c6f5804240d4d
ngugimuchangi <tech.curious14@gmail.com>
2022-05-10 15:47:44 +0000

    Fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		return (ptr);
 	}
-	else if (new_size == 0)
+	else if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
