7093e726cf808f08a63375bea6695d68b25a3942
ngugimuchangi <tech.curious14@gmail.com>
2022-05-10 14:13:49 +0000

    Fixed realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,15 +14,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
+		return (ptr);
 	}
-	if (ptr == NULL && new_size == 0)
+	else if (ptr == NULL && new_size == 0)
+	{
 		return (NULL);
-	if (ptr != NULL && new_size == 0)
+	}
+	else if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size > old_size || new_size < old_size)
+	else
 	{
 		free(ptr);
 		ptr = malloc(new_size);
