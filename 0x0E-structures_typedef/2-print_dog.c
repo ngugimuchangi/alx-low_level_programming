71687c24b8a0dae38ed7789d9ab6aba793d789f2
ngugimuchangi <tech.curious14@gmail.com>
2022-05-13 02:19:34 +0000

    Fixed nill to nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,12 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		(d->name == NULL) ? printf("Name: (nill)\n") :
-			printf("Name: %s\n", d->name);
-		(d->age == 0) ? printf("Age: (nill)\n") :
-			printf("Age: %f\n", d->age);
-		(d->owner == NULL) ? printf("Owner: (nill)\n") :
-			printf("Name: %s\n", d->owner);
-	}
+	if (d == NULL)
+		return;
+	(d->name == NULL) ? printf("Name: (nil)\n") :
+		printf("Name: %s\n", d->name);
+	(d->age == 0) ? printf("Age: (nil)\n") :
+		printf("Age: %f\n", d->age);
+	(d->owner == NULL) ? printf("Owner: (nil)\n") :
+		printf("Name: %s\n", d->owner);
 }
