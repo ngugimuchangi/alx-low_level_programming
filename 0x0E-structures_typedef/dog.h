c57b21b52f7455fc3befe9865cde235fdfc57260
ngugimuchangi <tech.curious14@gmail.com>
2022-05-13 01:36:46 +0000

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - consolidates attributes of dog
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
+
