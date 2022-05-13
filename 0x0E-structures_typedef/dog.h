0af8bcc2869c55e2511d28a20179217994e38698
ngugimuchangi <tech.curious14@gmail.com>
2022-05-13 01:59:46 +0000

    initalize struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,9 +3,9 @@
 
 /**
  * struct dog - consolidates attributes of dog
- * @name: dog name
- * @owner: dog owner
- * @age: dog age
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
  */
 typedef struct dog
 {
@@ -13,6 +13,10 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
 
