b3fc71ef6e6da7bcfb14c20f3ec78fa9f92a6bc8
ngugimuchangi <tech.curious14@gmail.com>
2022-05-16 11:07:39 +0000

    Check first occurrence of integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searchers through an array of integers
+ * @array: pointer to first element of array to check
+ * @size: size of array
+ * @cmp: pointer to callback comparison functions
+ * Return: index to first match on success, -1 on failure or
+ * no match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array)
+		if (cmp)
+			for (i = 0; i < size; i++)
+				if (cmp(array[i]))
+					break;
+	return (i);
+}
