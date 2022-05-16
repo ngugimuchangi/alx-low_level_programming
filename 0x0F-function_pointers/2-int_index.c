e3d8c11a848fe5d9c0745a3a1de2bb1eaaec6019
ngugimuchangi <tech.curious14@gmail.com>
2022-05-16 11:09:56 +0000

    Check alternate return values

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array)
 		if (cmp)
 			for (i = 0; i < size; i++)
-				if (cmp(array[i]))
+				if (cmp(array[i]) != 0)
 					break;
 	return (i);
 }
