48723e3bd8b23feaf9dd443a148499805d73fe0d
ngugimuchangi <tech.curious14@gmail.com>
2022-05-16 10:26:57 +0000

    Array iterator

---
+++ 0x0F-function_pointers/1-array-iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array-iterator - moves through array elements calling back to
+ * other functions
+ * @array: pointer to first element of array
+ * @size: size of array
+ * @action: pointer to callback functions
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && size != 0)
+		if (action)
+			for (i = 0; i < size; i++)
+				action(array[i]);
+}
