0c1272d2cc2f7ffdf32a813c7b8d596b7e61f0d8
ngugimuchangi <tech.curious14@gmail.com>
2022-05-17 19:17:13 +0000

    Print anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * p_char - prints a character
+ * @c_args: va_list to check for char specfier
+ * Return: nothing
+ */
+void p_char(va_list c_args)
+{
+	printf("%c", va_arg(c_args, int));
+}
+/**
+ * p_int - prints an integer
+ * @i_args: va_list to check for integer specifier
+ * Return: nothing
+ */
+void p_int(va_list i_args)
+{
+	printf("%i", va_arg(i_args, int));
+}
+/**
+ * p_float - prints a float
+ * @f_args: va_list to check for float specifier
+ * Return: nothing
+ */
+void p_float(va_list f_args)
+{
+	printf("%f", va_arg(f_args, double));
+}
+/**
+ * p_string - prints a string
+ * @s_args: va_list to check for string specifier
+ * Return: nothing
+ */
+void p_string(va_list s_args)
+{
+	char *s;
+
+	s = va_arg(s_args, char *);
+	if (!s)
+		s = "(nill)";
+	printf("%s", s);
+}
+
+/**
+ * print_all - prints characters based on format specifier
+ * @format: format string
+ * Decription: function that prints chracters based on specified
+ * format, i.e. c for char, i for integer, f for float and s
+ * for char. It ignores other characters in format string
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	p_func func[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_string},
+		{NULL, NULL}
+	};
+	int i, j;
+	char *delimiter = "";
+	va_list args;
+
+	va_start(args, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (func[j].sp != NULL)
+		{
+			if (*(func[j].sp) == format[i])
+			{
+				printf("%s", delimiter);
+				func[j].f(args);
+				delimiter = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct p_func - contains format specifiers and pointer to function
+ * @sp: format specifier string
+ * @f: pointer to function
+ * Description: the struct holds a format specifier and pointer to function
+ * used to select the right function to execute printing to stdout
+ */
+typedef struct p_func
+{
+	char *sp;
+	void (*f)(va_list);
+} p_func;
 
 #endif
