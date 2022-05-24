4ab51e61b40cd79f30f4bf2312e752b722fea121
ngugimuchangi <tech.curious14@gmail.com>
2022-05-24 16:53:58 +0000

    Print data within nodes of list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - prints the data in a node withing singly linked list
+ * @h: pointer to the head of the single list.
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 1;
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("%s\n", h->str);
+	if (h->next != NULL)
+		print_list(h->next);
+	i++;
+	return (i);
+}
