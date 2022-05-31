1faa7368c83330a625f671d58b029e0a87fc4d07
ngugimuchangi <tech.curious14@gmail.com>
2022-05-31 09:03:01 +0000

    Convert binary string to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary string to unsigned int
+ * @b: binary string to convert
+ *
+ * Return: converted unsigned int on success, 0 if string
+ * is NULL or contains non-binary characters.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j, k;
+	unsigned int res = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0, k = 1; b[i]; i++, k *= 2)
+		if (b[i] < 48 || b[i] > 49)
+			return (0);
+
+	for (j = 0, k /= 2; j < i; j++, k /= 2)
+		res += (b[j] - 48) * k;
+	return (res);
+}
