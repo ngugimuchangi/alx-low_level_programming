ff08f7f6992d886255eb3fe4fe6e65090a99bfc2
ngugimuchangi <tech.curious14@gmail.com>
2022-05-31 11:15:01 +0000

    Find nth bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - get the bit at a specific index.
+ * @n: number to check
+ * @index: position to check.
+ *
+ * Return: bit at given index on success, -1 on failuire
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i, res;
+
+	for (i = 0; i <= index; i++, n = n >> 1)
+	{
+		if (n == 0 && i < index)
+			return (-1);
+		res = (n & 1) ? 1 : 0;
+	}
+	return (res);
+}
