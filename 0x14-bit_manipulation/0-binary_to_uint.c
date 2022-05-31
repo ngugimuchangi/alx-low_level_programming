15551cba881ab0d503cdac30667537e22e6f3f64
ngugimuchangi <tech.curious14@gmail.com>
2022-05-31 16:21:14 +0000

    Errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,12 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, j, k;
+	int i, j, k = 1;
 	unsigned long int res = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0, k = 1; b[i]; i++, k *= 2)
+	for (i = 0; b[i]; i++, k *= 2)
 		if (b[i] < '0' || b[i] > '1' || i > 63)
 			return (0);
 	for (j = 0, k /= 2; j < i; j++, k /= 2)
