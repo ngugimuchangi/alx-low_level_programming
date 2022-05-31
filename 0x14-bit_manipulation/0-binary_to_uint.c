08b31de39d924690c4772c9bf37f5b6fda3de885
ngugimuchangi <tech.curious14@gmail.com>
2022-05-31 14:28:52 +0000

    Check for binary strings exceeding 64 bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 	for (i = 0, k = 1; b[i]; i++, k *= 2)
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] < '0' || b[i] > '1' || i > 63)
 			return (0);
 	for (j = 0, k /= 2; j < i; j++, k /= 2)
 		res += (b[j] - 48) * k;
