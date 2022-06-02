9385e9cd69eb233e17da9b4868fc69e5f89f55ba
ngugimuchangi <tech.curious14@gmail.com>
2022-06-02 16:04:55 +0000

    Comments

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 			S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
+	/* check if string is empty */
 	if (!text_content)
 	{
 		close(fd);
