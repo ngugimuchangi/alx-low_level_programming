2c396fedd93ce64a3dac0ba18725e31af3dba51e
ngugimuchangi <tech.curious14@gmail.com>
2022-06-02 14:52:06 +0000

    Added comments for future guide

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,19 +12,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buff;
 
+	/* check if filename is empty */
 	if (!filename)
 		return (0);
+	/* allocate memory to store read chracters */
 	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
+	/* open file */
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
+	/* read and close file */
 	i = read(fd, buff, letters);
 	if (i < 0)
 		return (0);
 	close(fd);
+	/* write to stdout */
 	j = write(STDOUT_FILENO, buff, i);
+	/* check if bytes written are as expected */
 	if (j < i)
 		return (0);
+	/* free memory */
 	free(buff);
 	return (i);
 }
