0a7e516d2908f95c85df6f12ba7262a480c03693
ngugimuchangi <tech.curious14@gmail.com>
2022-06-03 00:01:10 +0000

    Restructured functions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,83 @@
 #include <stdarg.h>
 #include "main.h"
 /**
- * copy - copies content of one file
- * @file_from: file to copy content
- * @buff: buffer to store copied content
+ * err_98 - displays error for copy (1st) file
+ * @i: return value of open or read calls
+ * @file_from: copy file
  *
- * Return: numer of copied characters
+ * Return: nothing
  */
-int copy(char *file_from, char *buff)
+void err_98(ssize_t i, char *file_from)
 {
-	int fd;
-	ssize_t i, j;
-
-	fd = open(file_from, O_RDONLY);
-	if (fd < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	i = read(fd, buff, 1024);
 	if (i < 0)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	j = close(fd);
-	if (j < 0)
+}
+/**
+ * err_99 - displays error for paste (2nd) file
+ * @i: return value for open and write calls
+ * @file_to: paste file
+ *
+ * Return: nothing
+ */
+void err_99(ssize_t i, char *file_to)
+{
+	if (i < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+}
+/**
+ * err_100 - displays errors for closing files
+ * @i: return value for close call
+ * @fd: file descriptor of file being closed
+ *
+ * Return: nothing
+ */
+void err_100(ssize_t i, int fd)
+{
+	if (i < 0)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-	return (i);
 }
 /**
- * paste - pastes copied content
+ * copy_paste - copies and pastes file contents
  * @file_from: file to copy content
  * @file_to: file to paste content
  *
  * Return: nothing
  */
-void paste(char *file_from, char *file_to)
+void copy_paste(char *file_from, char *file_to)
 {
-	int fd;
-	ssize_t i, j, k = 1024;
+	int fd, fd2;
+	ssize_t i = 1024, j, k;
 	char buff[1024];
 
-	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+	fd = open(file_from, O_RDONLY);
+	err_98(fd, file_from);
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
 			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd < 0)
+	err_99(fd2, file_to);
+	while (i == 1024)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	while (k == 1024)
-	{
-		k = copy(file_from, buff);
-		i = write(fd, buff, k);
-		if (i < 0)
+		i = read(fd, buff, 1024);
+		err_98(i, file_from);
+		j = write(fd2, buff, i);
+		if (j < 0 || j < i)
 		{
 			dprintf(2, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
-	j = close(fd);
-	if (j < 0)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+	k = close(fd);
+	err_100(k, fd);
+	k = close(fd2);
+	err_100(k, fd2);
 }
 /**
  * main - Entry point
@@ -88,6 +98,6 @@ int main(int argc, char **argv)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	paste(argv[1], argv[2]);
+	copy_paste(argv[1], argv[2]);
 	return (0);
 }
