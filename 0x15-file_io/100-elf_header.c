cce20a33a06e99a1fe305e36021a7ee208cd4962
ngugimuchangi <tech.curious14@gmail.com>
2022-06-04 15:15:39 +0000

    Included more OS options

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -98,6 +98,10 @@ void posabi(char *__buf)
 		printf("UNIX - System V\n");
 	else if (os == 2)
 		printf("UNIX - NetBSD\n");
+	else if (os == 3)
+		printf("UNIX - Linux\n");
+	else if (os == 4)
+		printf("UNIX - GNU\n");
 	else if (os == 6)
 		printf("UNIX - Solaris\n");
 	else
