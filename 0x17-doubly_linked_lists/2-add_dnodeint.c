01d868e0f69afa7483af841464dc8a7b205635e8
ngugimuchangi <tech.curious14@gmail.com>
2022-07-07 22:38:47 +0000

    null head pointers

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,19 +13,13 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *temp = *head;
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = NULL;
-	if (!head || !temp)
-	{
-		*head = new;
-		return (new);
-	}
 	new->next = *head;
 	*head = new;
 	return (new);
