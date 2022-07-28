ffaf1dfeb1c3c79db75a4ca7852ef751fc2a6a2f
ngugimuchangi <tech.curious14@gmail.com>
2022-07-28 22:25:11 +0000

    Delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * free_list - frees a linked list in hash table
+ * @head: head of linked list
+ *
+ * Return: nothing
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp = head;
+
+	while (temp)
+	{
+		head = temp->next;
+		free(temp);
+		temp = head;
+	}
+}
+
+/**
+ * hash_table_delete - deletes hash tables
+ * @ht: address of hash table
+ *
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+
+	if (!ht)
+		return;
+	for (; index < ht->size; index++)
+		if (ht->array[index])
+			free_list(ht->array[index]);
+	free(ht);
+}
