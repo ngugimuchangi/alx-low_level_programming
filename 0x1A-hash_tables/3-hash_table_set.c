9cec00bfb308132e3d18fa50f69cc891b1c8b934
ngugimuchangi <tech.curious14@gmail.com>
2022-07-28 19:39:56 +0000

    Don't create new hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -51,7 +51,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key || strcmp(key, "") == 0)
 		return (0);
 	if (!ht)
-		ht = hash_table_create(1);
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	add_node(&(ht->array[index]), key, value);
 	return (1);
