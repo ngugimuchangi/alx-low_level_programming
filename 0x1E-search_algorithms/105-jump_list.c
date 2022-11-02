504ed34d667abf929eeaeb7206381a69c01f8d5c
ngugimuchangi <tech.curious14@gmail.com>
2022-11-02 14:15:06 +0000

    betty fix

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -35,7 +35,8 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 			curr = curr->next;
 		}
 		last_idx = (jump < size - 1) ? jump : size - 1;
-		printf("Value found between indexes [%lu] and [%lu]\n", prev->index, last_idx);
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				prev->index, last_idx);
 
 		while (prev && prev->index <= last_idx)
 		{
